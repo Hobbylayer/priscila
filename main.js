9012ae611788082ac9cd3dc8a86d4ae06ba05569
Jorge Fernandez <jorge.fernandez@alegra.com>
2023-06-23 20:49:30 -0400

    add squirrel configuration

--- main.js
+++ main.js
@@ -1,52 +1,62 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const url = require("url");
-const db = require("./src/server/models/index");
-const { settings } = require("./settings");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+const db = require('./src/server/models/index');
+const { settings } = require('./settings');
 
 const appPath = path.dirname(app.getAppPath());
 process.env.APP_PATH = appPath;
 
 const isDev = settings.isDev;
 
+if (require('electron-squirrel-startup')) app.quit();
+
+if (process.platform === 'win32') {
+	app.setLoginItemSettings({
+		openAtLogin: true,
+		path: process.execPath,
+		args: ['--processStart', `"${process.execPath}"`, '--', 'start'],
+	});
+}
+
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "./preload.js"),
-      nodeIntegration: true,
-    },
-  });
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, './preload.js'),
+			nodeIntegration: true,
+		},
+	});
 
-  const startUrl = url.format({
-    pathname: path.join(__dirname, "./index.html"),
-    protocol: "file",
-    slashes: true,
-  });
+	const startUrl = url.format({
+		pathname: path.join(__dirname, './index.html'),
+		protocol: 'file',
+		slashes: true,
+	});
 
-  console.log(startUrl);
+	console.log(startUrl);
 
-  if (isDev) win.loadURL("http://localhost:5173/");
-  else win.loadURL(startUrl);
+	if (isDev) win.loadURL('http://localhost:5173/');
+	else win.loadURL(startUrl);
 };
 
 app.whenReady().then(async () => {
-  try {
-    await db.sequelize.authenticate();
-    await db.sequelize.sync();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+	try {
+		await db.sequelize.authenticate();
+		await db.sequelize.sync();
+		console.log('Connection has been established successfully.');
+	} catch (error) {
+		console.error('Unable to connect to the database:', error);
+	}
 
-  require("./src/server/index");
-  createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+	require('./src/server/index');
+	createWindow();
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
 });
